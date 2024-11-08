cf9547e5789d748103cc4d23bc55e9b723fd05c3
Eliah Kagan <degeneracypressure@gmail.com>
2024-11-08 15:03:53 -0500

    Check the specific Windows `GIX_TEST_IGNORE_ARCHIVES=1` failures
    This modifies the `test-fixtures-windows` job that tests on Windows
    with `GIX_TEST_IGNORE_ARCHIVES=1` so that, instead of checking that
    no more than 14 failures occur, it checks that the failing tests
    are exactly those that are documented in #1358 as expected to fail.
    
    The initial check that no tests have *error* status is preserved,
    with only stylistic changes, and kept separate from the subsequent
    logic so that the output is clearer.
    
    The new steps are no longer conditional on `nextest` having exited
    with a failure status, since (a) that was probably unnecessary
    before and definitely unnecessary now, (b) at last for now, the
    comparison is precise, so it would be strange to pass if the diff
    were to have changes on *all* lines, and (c) this makes it slightly
    less likely that #1358 will accidentally stay open even once fixed.
    
    The current approach is to actually retrieve the list of tests
    expected to fail on Windows with `GIX_TEST_IGNORE_ARCHIVES=1` from
    the #1358 issue body. This has the advantage that it automatically
    keeps up to date with changes made to that issue description, but
    this is of course not the only possible approach for populating the
    expected value.
    
    Two changes should be made before this is ready:
    
    - As noted in the "FIXME" comment, the job should currently fail
      becuase the performance test reported to fail in #1358 is not
      being filtered out from the expected failures list. It's left in
      as of this commit, to verify that the job is capable of failing.
    
      (After that, the performance test should either be filtered out
      or removed from the list in #1358, but the former approach is
      currently preferable because I have not done diverse enough
      testing to check if the failure on my main Windows system is due
      to that system being too slow rather than a performance bug.)
    
    - The scratchwork file should be removed once no longer needed.

---
+++ scratchwork.ps1
@@ -0,0 +1,43 @@
+# Omit from all script steps, becuase GHA prepends it.
+$ErrorActionPreference = 'stop'
+
+
+Write-Output '====== Output of scratchwork for "Collect actual failures" step: ======'
+
+[xml]$junit_xml = Get-Content 'target/nextest/with-xml/junit.xml'
+
+$actual_failures = $junit_xml.SelectNodes("//testcase[failure]") |
+    ForEach-Object { "$($_.classname) $($_.name)" } |
+    Sort-Object
+
+Write-Output $actual_failures
+Set-Content -Path 'actual-failures.txt' -Value $actual_failures
+
+
+Write-Output '====== Output of scratchwork for "Collect expected failures" step: ======'
+
+$issue = 1358  # https://github.com/GitoxideLabs/gitoxide/issues/1358
+
+$match_info = gh issue --repo GitoxideLabs/gitoxide view $issue --json body --jq .body |
+    Out-String |
+    Select-String -Pattern '(?s)```text\r?\n(.*?)```'
+
+# FIXME: Check that the diff can fail, then filter out performance tests in Where-Object.
+$expected_failures = $match_info.Matches.Groups[1].Value -split "`n" |
+    Where-Object { $_ -match '^\s*FAIL \[' } |
+    ForEach-Object { $_ -replace '^\s*FAIL \[\s*\d+\.\d+s\]\s*', '' -replace '\s+$', '' } |
+    Sort-Object
+
+Write-Output $expected_failures
+Set-Content -Path 'expected-failures.txt' -Value $expected_failures
+
+
+Write-Output '====== Output of scratchwork for "Compare expected and actual failures" step: ======'
+
+# Fail the check if there are any differences, even unexpectedly passing tests, so they can be
+# investigated. (If this check is made blocking for PRs, this exact check may need to be changed.)
+git --no-pager diff --no-index --exit-code -U1000000 -- expected-failures.txt actual-failures.txt
+
+
+# Omit from script steps, because GHA appends it.
+if ((Test-Path -LiteralPath variable:\LASTEXITCODE)) { exit $LASTEXITCODE }
