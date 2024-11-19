63978f6e54cf48e7b5afe65dede851890a163ebd
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-11-19 11:27:36 +0100

    feat: `entry::Stage` can now be converted into `entry::Flags`.
    This makes it easier to change the stage of an entry.

--- gix-index/tests/index/entry/mod.rs
+++ gix-index/tests/index/entry/mod.rs
@@ -1,3 +1,16 @@
+mod flags {
+    use gix_index::entry::{Flags, Stage};
+
+    #[test]
+    fn from_stage() {
+        for stage in [Stage::Unconflicted, Stage::Base, Stage::Ours, Stage::Theirs] {
+            let actual = Flags::from_stage(stage);
+            assert_eq!(actual.stage(), stage);
+            let actual: Flags = stage.into();
+            assert_eq!(actual.stage(), stage);
+        }
+    }
+}
 mod mode;
 mod stat;
 mod time;
