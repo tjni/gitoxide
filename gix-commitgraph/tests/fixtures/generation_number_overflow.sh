2a242503d22cfa6311ef1ad01a84904a025719f2
Eliah Kagan <degeneracypressure@gmail.com>
2024-11-23 02:54:32 -0500

    Manually create tag if 32-bit `git` rejects it
    This is a workaround for the problem where `git tag` rejects the
    timestamp `4147483646` from `generation_number_overflow.sh` as out
    of range, even though it has already been accepted to create a
    commit and `git tag` is obtaining it from the commit metadata.
    
    The test already passed if run without `GIX_TEST_IGNORE_ARCHIVES`,
    even though the commit and tag metadata in the committed archive
    repo has this timestamp interpreted as from 1970 rather than in the
    far future. The change here just allows the fixture script to
    create the repository on a 32-bit system where `git` would refuse
    to make the tag, by manually creating a loose tag. (Creating a
    packed tag would also work, but would not improve the ambiguity.)

--- gix-commitgraph/tests/fixtures/generation_number_overflow.sh
+++ gix-commitgraph/tests/fixtures/generation_number_overflow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-function tick () {
+function tick() {
   if test -z "${tick+set}"
   then
     tick=1112911993
@@ -13,9 +13,25 @@ function tick () {
   export GIT_COMMITTER_DATE GIT_AUTHOR_DATE
 }
 
-tick
-function commit() {
-  local message=${1:?first argument is the commit message}
+function force_tag() {
+  local name head_oid common_dir
+  name=${1:?argument the tag name}
+
+  # This should only be needed with 32-bit `git`, so fail otherwise.
+  word_size="$(
+    git --version --build-options |
+      awk '$1 == "sizeof-size_t:" { print $2 }'
+  )"
+  ((word_size == 4))
+
+  # Manually create the tag.
+  head_oid="$(git rev-parse HEAD)"
+  common_dir="$(git rev-parse --git-common-dir)"
+  (set -o noclobber; echo "$head_oid" > "$common_dir/refs/tags/$name")
+}
+
+function tagged_commit() {
+  local message=${1:?first argument is the commit message and tag name}
   local date=${2:-}
   local file="$message.t"
   echo "$1" > "$file"
@@ -26,8 +42,10 @@ function commit() {
     tick
   fi
   git commit -m "$message"
-  git tag "$message"
+  git tag -- "$message" || force_tag "$message"
 }
+
+tick
 
 # adapted from git/t/t5318 'lower layers have overflow chunk'
 UNIX_EPOCH_ZERO="@0 +0000"
@@ -36,13 +54,12 @@ FUTURE_DATE="@4147483646 +0000"
 git init
 git config commitGraph.generationVersion 2
 
-set -x
-commit future-1 "$FUTURE_DATE"
-commit old-1 "$UNIX_EPOCH_ZERO"
+tagged_commit future-1 "$FUTURE_DATE"
+tagged_commit old-1 "$UNIX_EPOCH_ZERO"
 git commit-graph write --reachable
-commit future-2 "$FUTURE_DATE"
-commit old-2 "$UNIX_EPOCH_ZERO"
+tagged_commit future-2 "$FUTURE_DATE"
+tagged_commit old-2 "$UNIX_EPOCH_ZERO"
 git commit-graph write --reachable --split=no-merge
-commit extra
+tagged_commit extra
 # this makes sure it's actually in chain format.
 git commit-graph write --reachable --split=no-merge
