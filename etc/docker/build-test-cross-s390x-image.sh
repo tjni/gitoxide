3a296aefa67f8983beeba3b717e50f2904b35ca2
Eliah Kagan <degeneracypressure@gmail.com>
2025-03-04 20:03:17 +0000

    Add `jq` to container; simplify container build command
    Installing `jq` in the container allows the `ask::askpass_only` and
    `ask::username_password` to pass (they use `jq`).

--- etc/docker/build-test-cross-s390x-image.sh
+++ etc/docker/build-test-cross-s390x-image.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-set -e
-mkdir empty-context
-docker build -f etc/docker/Dockerfile.test-cross-s390x \
-    -t cross-rs-gitoxide/s390x-unknown-linux-gnu empty-context
+docker build -t cross-rs-gitoxide/s390x-unknown-linux-gnu - <etc/docker/Dockerfile.test-cross-s390x
