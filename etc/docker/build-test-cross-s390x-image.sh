2a155b686c48e4cadbb395dbd86177be520cb05f
Eliah Kagan <degeneracypressure@gmail.com>
2025-03-04 22:17:16 +0000

    Put experiment details (except dockerfile) in a script

--- etc/docker/build-test-cross-s390x-image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build -t cross-rs-gitoxide/s390x-unknown-linux-gnu - <etc/docker/Dockerfile.test-cross-s390x
---
+++ etc/run-cross-experiment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -ex
+
+# Build the customized `cross` container image.
+docker build -t cross-rs-gitoxide/s390x-unknown-linux-gnu \
+    - <etc/docker/Dockerfile.test-cross-s390x
+
+# Clean files that could cause tests to wrongly pass or fail.
+cargo clean
+gix clean -xd -m '*generated*' -e
+
+# Run the test suite.
+cross test --workspace --no-fail-fast --target s390x-unknown-linux-gnu \
+    --no-default-features --features max-pure \
+    -- --skip realpath::fuzzed_timeout
