720a23f873508fc574c0f0e9b212dbd0dd76b8bb
Eliah Kagan <degeneracypressure@gmail.com>
2025-03-16 09:04:11 -0400

    feat: Add `jtt bash-program` (`jtt bp`) to show `bash_program()`
    This adds a `bash-program` subcommand, abbreviated `bp`, to the
    `gix-testools` program (`jtt`) to check what the `bash_program()`
    library function gives.
    
    This is intended for diagnostic use and should probably not be used
    in scripting. Currently it shows the quoted debug repreesentation
    of the path.

--- tests/tools/src/main.rs
+++ tests/tools/src/main.rs
@@ -1,5 +1,14 @@
 use std::{fs, io, io::prelude::*, path::PathBuf};
 
+fn bash_program() -> io::Result<()> {
+    use std::io::IsTerminal;
+    if !std::io::stdout().is_terminal() {
+        eprintln!("warning: `bash-program` subcommand not meant for scripting, format may change");
+    }
+    println!("{:?}", gix_testtools::bash_program());
+    Ok(())
+}
+
 fn mess_in_the_middle(path: PathBuf) -> io::Result<()> {
     let mut file = fs::OpenOptions::new().read(false).write(true).open(path)?;
     file.seek(io::SeekFrom::Start(file.metadata()?.len() / 2))?;
@@ -17,6 +26,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args().skip(1);
     let scmd = args.next().expect("sub command");
     match &*scmd {
+        "bash-program" | "bp" => bash_program()?,
         "mess-in-the-middle" => mess_in_the_middle(PathBuf::from(args.next().expect("path to file to mess with")))?,
         #[cfg(unix)]
         "umask" => umask()?,
