a845a4b5b0579cd65f1e2f5c18a751329ff9504f
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-07-04 12:15:55 +0200

    Make output more verbose

--- gitoxide-core/src/repository/tag.rs
+++ gitoxide-core/src/repository/tag.rs
@@ -5,12 +5,26 @@ pub fn list(repo: gix::Repository, out: &mut dyn std::io::Write) -> anyhow::Resu
         let tag = reference.peel_to_tag();
         let tag_ref = tag.as_ref().map(gix::Tag::decode);
 
-        let name = match tag_ref {
-            Ok(Ok(tag)) => tag.name.to_string(),
-            _ => reference.name().shorten().to_string(),
-        };
+        // `name` is the name of the file in `refs/tags/`. This applies to both lightweight as well
+        // as annotated tags.
+        let name = reference.name().shorten();
 
-        writeln!(out, "{name}")?;
+        match tag_ref {
+            Ok(Ok(tag_ref)) => {
+                // `tag_name` is the name provided by the user via `git tag -a/-s/-u`. It is only
+                // present for annotated tags.
+                let tag_name = tag_ref.name;
+
+                if name == tag_name {
+                    writeln!(out, "{name} *")?;
+                } else {
+                    writeln!(out, "{name} [tag name: {}]", tag_ref.name)?;
+                }
+            }
+            _ => {
+                writeln!(out, "{name}")?;
+            }
+        }
     }
 
     Ok(())
