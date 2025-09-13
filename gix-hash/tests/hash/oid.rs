58e2d18cd0684f2da51f94eb51e4402165e63efa
Sebastian Thiel <sebastian.thiel@icloud.com>
2025-09-13 18:54:42 +0200

    refactor

--- gix-hash/tests/hash/oid.rs
+++ gix-hash/tests/hash/oid.rs
@@ -22,12 +22,10 @@ fn is_null() {
 
 #[test]
 fn is_empty_blob() {
-    // Test with ObjectId::empty_blob
     let empty_blob = gix_hash::ObjectId::empty_blob(gix_hash::Kind::Sha1);
     assert!(empty_blob.is_empty_blob());
     assert!(empty_blob.as_ref().is_empty_blob());
-    
-    // Test that non-empty blob hash returns false
+
     let non_empty = gix_hash::Kind::Sha1.null();
     assert!(!non_empty.is_empty_blob());
     assert!(!non_empty.as_ref().is_empty_blob());
@@ -35,28 +33,11 @@ fn is_empty_blob() {
 
 #[test]
 fn is_empty_tree() {
-    // Test with ObjectId::empty_tree
     let empty_tree = gix_hash::ObjectId::empty_tree(gix_hash::Kind::Sha1);
     assert!(empty_tree.is_empty_tree());
     assert!(empty_tree.as_ref().is_empty_tree());
-    
-    // Test that non-empty tree hash returns false
+
     let non_empty = gix_hash::Kind::Sha1.null();
     assert!(!non_empty.is_empty_tree());
     assert!(!non_empty.as_ref().is_empty_tree());
-}
-
-#[test]
-fn oid_methods_are_consistent_with_objectid() {
-    // Verify that the oid methods return the same results as ObjectId methods
-    let empty_blob = gix_hash::ObjectId::empty_blob(gix_hash::Kind::Sha1);
-    let empty_tree = gix_hash::ObjectId::empty_tree(gix_hash::Kind::Sha1);
-    
-    // Check that ObjectId and oid versions give same results
-    assert_eq!(empty_blob.is_empty_blob(), empty_blob.as_ref().is_empty_blob());
-    assert_eq!(empty_tree.is_empty_tree(), empty_tree.as_ref().is_empty_tree());
-    
-    // Check cross-validation (empty blob is not empty tree and vice versa)
-    assert!(!empty_blob.as_ref().is_empty_tree());
-    assert!(!empty_tree.as_ref().is_empty_blob());
 }
