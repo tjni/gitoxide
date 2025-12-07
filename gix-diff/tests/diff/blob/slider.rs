cb66d197fb1bc75813ce1079d43e3870dffa055c
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-12-07 09:21:13 +0100

    Account for "\ No newline at end of file"

--- gix-diff/tests/diff/blob/slider.rs
+++ gix-diff/tests/diff/blob/slider.rs
@@ -213,7 +213,20 @@ mod baseline {
                         hunk_lines.extend_from_slice(line);
                         hunk_lines.push(b'\n');
                     }
-                    _ => unreachable!("BUG: expecting unified diff format"),
+                    b'\\' => {
+                        if line == "\\ No newline at end of file".as_bytes() {
+                            // Do nothing.
+                        } else {
+                            unreachable!(
+                                "BUG: expecting unified diff format, found line: `{}`",
+                                line.to_str_lossy()
+                            )
+                        }
+                    }
+                    _ => unreachable!(
+                        "BUG: expecting unified diff format, found line: `{}`",
+                        line.to_str_lossy()
+                    ),
                 }
 
                 match self.lines.peek() {
