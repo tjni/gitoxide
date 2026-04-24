78b7fdf7e99482cbd8755534bc5dddb06e2bb24f
Christoph Rüßler <christoph.ruessler@mailbox.org>
2026-04-24 21:01:39 +0200

    Use source's object_hash in TryFrom

--- gix-odb/src/store_impls/dynamic/handle.rs
+++ gix-odb/src/store_impls/dynamic/handle.rs
@@ -349,7 +349,7 @@ impl TryFrom<&super::Store> for super::Store {
             &mut s.replacements(),
             crate::store::init::Options {
                 slots: crate::store::init::Slots::Given(s.files.len().try_into().expect("BUG: too many slots")),
-                object_hash: Default::default(),
+                object_hash: s.object_hash,
                 use_multi_pack_index: false,
                 alloc_limit_bytes: s.alloc_limit_bytes,
                 current_dir: s.current_dir.clone().into(),
